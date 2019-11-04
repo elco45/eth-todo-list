c94d33dbc4239c88be34f5587f6169057e68ec7e
elco45 <dennischeong2003@yahoo.es>
2019-11-04 15:29:10 -0500

    List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,5 +3,20 @@ pragma solidity ^0.5.0;
 contract TodoList {
   uint public taskCount = 0;
 
-  //16:10
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks; // hash <key => value>
+
+  constructor() public {
+    createTask("Hello Blockchain!");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
